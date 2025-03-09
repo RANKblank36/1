8be924356f6ccd4a20dae1400b79644baaee5929
RANKblank36 <tendosana@gmail.com>
2025-03-09 18:07:41 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+tuple1 = (10, "Hello", 3.14, True, [1, 2, 3])
+print("Tuple with different data types:", tuple1)
+
+tuple2 = (1, 2, 3, 4, 5)
+print("Tuple with integers:", tuple2)
+
+tuple3 = tuple2 + (9,)
+print("New tuple after adding 9:", tuple3)
+
+count_3 = tuple3.count(3)
+print("Count of 3 in tuple:", count_3)
+
